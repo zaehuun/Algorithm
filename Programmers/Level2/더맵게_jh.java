c4b4403cbc5c4e29bcd71c9e3535804994237067
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-08-03 01:31:04 +0900

    PriorityQueue / Collections.reverseOrder()

---
+++ Programmers/Level2/더맵게_jh.java
@@ -0,0 +1,19 @@
+/*
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int sc : scoville) pq.offer(sc);
+        while(pq.size() != 0){
+            int first = pq.poll();
+            if(first >= K) return answer;
+            if (pq.size() == 0 && first < K) return -1;
+            int second = pq.poll();
+            pq.offer(first + second * 2);
+            answer += 1;
+        }
+        return answer;
+    }
+}
+*/
