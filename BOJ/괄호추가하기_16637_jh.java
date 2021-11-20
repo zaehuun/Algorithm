b880fe56beb73f77bb69f725c6e577d93e096c65
김재훈 (Kim JaeHoon) <51132077+zaehuun@users.noreply.github.com>
2021-11-21 05:25:50 +0900

    수식 괄호 / DFS

--- BOJ/괄호추가하기_16637_jh.java
+++ BOJ/괄호추가하기_16637_jh.java
@@ -1,5 +1,3 @@
-package com.example.demo;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,20 +5,44 @@ import java.util.*;
 
 
 public class Main {
+	static List<Integer> number = new ArrayList<>();
+	static List<Character> op = new ArrayList<>();
+	static int answer = Integer.MIN_VALUE;
+	public static int calc(char c, int a, int b){
+		if(c == '+') return a + b;
+		if(c == '-') return a - b;
+		if(c == '*') return a * b;
+		return 0;
+	}
+	public static void dfs(int idx, int sum){
+		if(idx >= op.size()){
+			answer = Math.max(answer,sum);
+			return;
+		}
 
+		int result = calc(op.get(idx), sum, number.get(idx+1));
+		dfs(idx+1,result);
+
+		if(idx+1<op.size()){
+			result = calc(op.get(idx+1), number.get(idx+1), number.get(idx+2));
+			result = calc(op.get(idx), sum, result );
+			dfs(idx+2, result);
+		}
+
+
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String st =br.readLine();
-		List<Integer> number = new ArrayList<>();
-		List<Character> op = new ArrayList<>();
+
 		for(char c : st.toCharArray()){
 			if(c == '+' || c == '-' || c== '*')
 				op.add(c);
 			else
 				number.add(c - '0');
 		}
-		System.out.println(number);
-		System.out.println(op);
+		dfs(0,number.get(0));
+		System.out.println(answer);
 	}
 }
