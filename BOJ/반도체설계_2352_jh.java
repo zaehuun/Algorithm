22a1f1fab2437c12cf5c518c41e17317f97ef9fb
김재훈 (Kim JaeHoon) <51132077+zaehuun@users.noreply.github.com>
2021-12-16 21:23:09 +0900

    LIS / 이분탐색

---
+++ BOJ/반도체설계_2352_jh.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+
+		List<Integer> list = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++){
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+
+		List<Integer> result = new ArrayList<>();
+		result.add(0);
+		for(int i = 0; i < n; i++){
+			int v = list.get(i);
+			if(result.get(result.size() - 1) < v){
+				result.add(v);
+			}
+			else{
+				int left = 1;
+				int right = result.size() - 1;
+
+				while(left<right){
+					int mid = (left + right) / 2;
+					if(result.get(mid) < v){
+						left = mid + 1;
+					}
+					else{
+						right = mid;
+					}
+				}
+				result.set(right, v);
+			}
+		}
+		System.out.println(result.size() - 1);
+	}
+
+}
