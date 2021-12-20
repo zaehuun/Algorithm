dfeac529a514dc541dbe7f7ca81e4710904b2cdf
김재훈 (Kim JaeHoon) <51132077+zaehuun@users.noreply.github.com>
2021-12-20 22:01:07 +0900

    거리 두기 / 대각선 / 중간

--- Programmers/Level2/거리두기확인하기_jh.java
+++ Programmers/Level2/거리두기확인하기_jh.java
@@ -1,62 +1,52 @@
-//경우의 수를 너무 복잡하게 생각하고 문제를 푸려고 한 거 같다...
-//꼭 다시 풀어봐야 함.
-
 class Solution {
-    
-    public boolean check(int y, int x, String[] arr){
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,1,-1};
+    int[] dy = {1,-1,0,0};
+    int[] dx = {0,0,1,-1};
+    int[] ddy = {2,-2,0,0};
+    int[] ddx = {0,0,2,-2};
+    int[] cy = {-1,-1,1,1};
+    int[] cx = {-1,1,-1,1};
+    public boolean check(int row, int col, String[] place){
+        //거리 1이 사람인 경우
         for(int i = 0; i < 4; i++){
-            int ty = y + dy[i];
-            int tx = x + dx[i];
-            if(ty < 0 || ty >= 5 || tx < 0 || tx >= 5) continue;
-            if(arr[ty].charAt(tx) == 'P') return true;
+            int ty = row + dy[i];
+            int tx = col + dx[i];
+            if(ty < 0 || tx < 0 || ty >= place.length || tx >= place[0].length()) continue;
+            if(place[ty].charAt(tx) == 'P') return false;
+        }
+        for(int i = 0; i < 4; i++){
+            int ty = row + ddy[i];
+            int tx = col + ddx[i];
+            if(ty < 0 || tx < 0 || ty >= place.length || tx >= place[0].length()) continue;
+            if(place[ty].charAt(tx) == 'P' && place[(ty+row) / 2].charAt((tx+col) / 2) == 'O') return false;
         }
         
-        dx = new int[]{-2,2,0,0};
-        dy = new int[]{0,0,-2,2};
         for(int i = 0; i < 4; i++){
-            int ty = y + dy[i];
-            int tx = x + dx[i];
-            if(ty < 0 || ty >= 5 || tx < 0 || tx >= 5) continue;
-            if(arr[ty].charAt(tx) == 'P' && arr[(ty+y)/2].charAt((tx+x)/2) != 'X') return true;
+            int ty = row + cy[i];
+            int tx = col + cx[i];
+            if(ty < 0 || tx < 0 || ty >= place.length || tx >= place[0].length()) continue;
+            if(place[ty].charAt(tx) == 'P' && (place[ty].charAt(col) == 'O' || place[row].charAt(tx) == 'O')) return false;
         }
         
-        dx = new int[]{1,1,-1,-1};
-        dy = new int[]{1,-1,1,-1}; // 4 2 8 10
-        for(int i = 0; i < 4; i++){
-            int ty = y + dy[i];
-            int tx = x + dx[i];
-            if(ty < 0 || ty >= 5 || tx < 0 || tx >= 5) continue;
-            if(arr[ty].charAt(tx) == 'P' && !(arr[y].charAt(tx) == 'X' && arr[ty].charAt(x) == 'X')) return true;
+        return true;
+    }
+    public boolean checkPlace(String[] places){
+        for(int row = 0; row < places.length; row++){
+            for(int col = 0; col < places[0].length(); col++){
+                if(places[row].charAt(col) == 'P'){
+                    if(!check(row,col,places)){
+                        return false;
+                    }
+                }
+            }
         }
-        
-        return false;
+        return true;
     }
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-        //p: 사람, 0: 빈 테이블, x: 파티션
         
         for(int i = 0; i < places.length; i++){
-            boolean flag = false;
-            for(int j = 0; j < places.length; j++){
-                for(int k = 0; k < places.length; k++){
-                    if(places[i][j].charAt(k) == 'P'){
-                        if(check(j,k,places[i])){
-                            flag = true;
-                            break;
-                        }
-                    }
-                }
-                if(flag){
-                    answer[i] = 0;
-                    break;
-                }
-            }
-            if(!flag){
-                answer[i] = 1;
-            }
-            
+            if(checkPlace(places[i])) answer[i] = 1;
+            else answer[i] = 0;
         }
         return answer;
     }
