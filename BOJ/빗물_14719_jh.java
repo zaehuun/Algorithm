20b45fa0ecec06f25b00a57f3cc3f48de9d33b7a
김재훈 (Kim JaeHoon) <51132077+zaehuun@users.noreply.github.com>
2022-01-06 07:31:49 +0900

    구현

--- BOJ/빗물_14719_jh.java
+++ BOJ/빗물_14719_jh.java
@@ -1,23 +1,40 @@
-package com.example.demo;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+	public static int[][] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int h = Integer.parseInt(st.nextToken());
 		int w = Integer.parseInt(st.nextToken());
+		arr = new int[h][w];
 
-		int[] block = new int[w];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < w; i++){
-			block[i] = Integer.parseInt(st.nextToken());
+			for(int j = h - Integer.parseInt(st.nextToken()); j < h; j++){
+				arr[j][i] = 1;
+			}
 		}
-		
+
+		int answer = 0;
+		for(int i = 0; i < h; i++){
+			boolean flag = false;
+			int count = 0;
+			for(int j = 0; j < w; j++){
+				if(arr[i][j] == 1){
+					flag = true;
+					answer += count;
+					count = 0;
+				}
+				else if(arr[i][j] == 0 && flag){
+					count++;
+				}
+			}
+		}
+		System.out.println(answer);
 	}
 }
 
