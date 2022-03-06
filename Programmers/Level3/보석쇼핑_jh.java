f780e790bff0d5edb0dcb8068de62b0720bb7dd8
김재훈 (Kim JaeHoon) <51132077+zaehuun@users.noreply.github.com>
2022-03-07 04:58:11 +0900

    슬라이딩 윈도우 / 투포인터

--- Programmers/Level3/보석쇼핑_jh.java
+++ Programmers/Level3/보석쇼핑_jh.java
@@ -1,46 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] gems) {
-        //R D E S
-        int[] answer = new int[2];
-        Set<String> gem = new HashSet<>();
+    
         Map<String, Integer> map = new HashMap<>();
-        
-        for(String g : gems) gem.add(g);
-        
-        int totalCnt = gem.size();
-        
+        int lidx = 0;
+        int ridx = 0;
         int left = 0;
         int right = 0;
-        int d = 100000;
+        int answer = Integer.MAX_VALUE;
+        Set<String> set = new HashSet<>();
+        for(String gem : gems){
+            set.add(gem);
+        }
         while(true){
-            // if(right == gems.length) break;
-            if(gem.size() == map.keySet().size()){ //모든 보석을 포함
-                if(right - left < d){
-                    d = right - left;
-                    answer[0] = left;
-                    answer[1] = right;
+            if(map.size() == set.size()){
+                if(answer > ridx - lidx){
+                    right = ridx;
+                    left = lidx;
+                    answer = ridx - lidx;
                 }
-                    
-                map.put(gems[left], map.get(gems[left]) - 1);
-                // List<String> li = new ArrayList<>();
-                // for(String key : map.keySet()){
-                //     if(map.get(key) == 0) li.add(key);
-                // }
-                // for(String key : li)
-                //     map.remove(key);
-                if(map.get(gems[left]) == 0)
-                    map.remove(gems[left]);
-                left += 1;
+                
+                map.put(gems[lidx], map.get(gems[lidx]) - 1);
+                if(map.get(gems[lidx]) == 0){
+                    map.remove(gems[lidx]);
+                }
+                lidx++;
             }
-            else if(right == gems.length) break;
-            else{ //아직 모든 보석 포함 x
-                if(!map.containsKey(gems[right])) map.put(gems[right], 0);
-                map.put(gems[right], map.get(gems[right]) + 1);
-                right += 1;
+            else if(ridx == gems.length) break;
+            else{
+                map.put(gems[ridx],map.getOrDefault(gems[ridx],0)+1);
+                ridx++;
             }
         }
-        answer[0] += 1;
-        return answer;
+        
+        int[] result = new int[2];
+        result[0] = left + 1;
+        result[1] = right;
+        return result;
     }
 }
